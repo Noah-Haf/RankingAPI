5b38659dcfb1add6c542985f600a85ced3a43041
Noah Hafner <noah.hafner@outlook.com>
2021-07-27 14:20:20 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import os
+import requests
+from ro_py.client import Client
+from dotenv import load_dotenv
+import asyncio
+load_dotenv()
+
+RobloxCookie = os.environ.get("COOKIE")
+APIKEY = os.environ.get("API_KEY")
+
+
+client = Client(RobloxCookie)
+
+app = FastAPI()
+
+
+@app.get("/group/promote/")
+async def read_items(user_name: str, key: str,groupid: int):
+    if key == APIKEY:
+     group = await client.get_group(groupid)
+     usernameinsystem = await client.get_user_by_username(user_name)
+     user_id = usernameinsystem.id
+     membertorank =  await group.get_member_by_id(user_id)
+     await membertorank.promote()
+     return ("The user was promoted!")
+    else:
+        return "Incorrect key"
+
+@app.get("/group/demote/")
+async def read_items(user_name: str, key: str, groupid: int):
+    if key == APIKEY:
+     group = await client.get_group(groupid)
+     usernameinsystem = await client.get_user_by_username(user_name)
+     user_id = usernameinsystem.id
+     membertorank =  await group.get_member_by_id(user_id)
+     await membertorank.demote()
+     return ("The user was demoted!")
+    else:
+        return "Incorrect key"
