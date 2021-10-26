9a045bf8c28c5b0ca20fe7f3ac9834993d8869d9
Cookie <61884850+CookieHax@users.noreply.github.com>
2021-10-26 16:39:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -56,3 +56,11 @@ async def read_items(key: str, groupid: int):
      return (group.member_count)
     else:
         return "Incorrect key"
+
+@app.get("/group/shout/")
+async def read_items(key: str, groupid: int):
+    if key == APIKEY:
+     group = await client.get_group(groupid)
+     return (group.shout)
+    else:
+        return "Incorrect key"
